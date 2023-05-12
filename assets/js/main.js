97d762e5beacbbaa1cf1355b5584e36a58d581de
Huzefa Mohammed Hussain <huzefamhd@gmail.com>
2023-05-12 15:14:08 +0400

    menu and mobile UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,74 @@ function myFunction() {
         x.style.display = "block";
     }
 }
+
+$(".menu-icon").click(function () {
+    $(".menu").toggleClass('show');
+});
+
+function showServices() {
+
+    // var html = `<div class="service-card">
+    //     <div class="info">
+    //         <div class="text">Some Text</div>
+    //         <button>Book Now</button>
+    //     </div>
+    //     <img src="assets/img/services/1.png" alt="">
+    // </div>`
+
+    for (i = 1; i <= 12; i++) {
+        $("#services").append(getServiceCard(i));
+
+    }
+}
+
+function getServiceCard(i) {
+    var title;
+    switch (i) {
+        case 1:
+            title = 'Blinds and Curtains'
+            break;
+        case 2:
+            title = 'Wall paint, Spray paint, Putty paint'
+            break;
+        case 3:
+            title = 'A/C Servicing'
+            break;
+        case 4:
+            title = 'Wooden flooring, SPC, PVC, Venyl'
+            break;
+        case 5:
+            title = 'POP and False Ceiling'
+            break;
+        case 6:
+            title = 'Fit-out from Shell and core core to complete finalization'
+            break;
+        case 7:
+            title = 'Hardwood works'
+            break;
+        case 8:
+            title = 'Designing 2D, 3D, AutoCAD'
+            break;
+        case 9:
+            title = 'Customised furniture'
+            break;
+        case 10:
+            title = 'Fixinng of wallpaper, wall sticker, WPC, Cladding'
+            break;
+        case 11:
+            title = 'Interior designing'
+            break;
+        case 12:
+            title = 'Equipment Installation'
+            break;
+
+    }
+
+    return `<div class="service-card">
+    <div class="info">
+        <div class="text">${title}</div>
+        <button>Book Now</button>
+    </div>
+    <img src="assets/img/services/${i}.png" alt="">
+</div>`;
+}
